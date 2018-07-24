1c1830e89c4ce72667095bb0549179a1011afbe9
Ian Carroll <ian.d.carroll@gmail.com>
2018-07-24 13:53:40 -0700

    Add a route that is handled by a hard-coded controller

--- ch1/hello_app/app/controllers/application_controller.rb
+++ ch1/hello_app/app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  def hello
+    render html: "By the bisected moons of Betelguese, I greet thee, World"
+  end
 end
